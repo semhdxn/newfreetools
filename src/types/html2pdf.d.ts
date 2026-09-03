1baf5ccf29d1b638aff80bce5e69eaab9a6777d2
Dan <dxn999@gmail.com>
2026-09-03 16:15:28 +0100

    newest

---
+++ src/types/html2pdf.d.ts
@@ -0,0 +1,33 @@
+declare module 'html2pdf.js' {
+  interface Html2PdfOptions {
+    margin?: number | number[];
+    filename?: string;
+    image?: {
+      type?: string;
+      quality?: number;
+    };
+    html2canvas?: {
+      scale?: number;
+      logging?: boolean;
+      useCORS?: boolean;
+      allowTaint?: boolean;
+    };
+    jsPDF?: {
+      orientation?: 'portrait' | 'landscape';
+      unit?: 'mm' | 'cm' | 'in' | 'px' | 'pc' | 'em' | 'ex';
+      format?: string;
+      compress?: boolean;
+    };
+  }
+
+  interface Html2Pdf {
+    set(options: Html2PdfOptions): Html2Pdf;
+    from(element: HTMLElement | string): Html2Pdf;
+    save(): Promise<void>;
+    output(type: string): Promise<void>;
+    then(callback: () => void): Html2Pdf;
+  }
+
+  function html2pdf(): Html2Pdf;
+  export default html2pdf;
+}
